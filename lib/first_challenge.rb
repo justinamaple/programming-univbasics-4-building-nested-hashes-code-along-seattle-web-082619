bac9a2a3e5c36b59adbf5c6bc7552a356c692e82
Justin Maple <justinamaple@gmail.com>
2019-08-18 21:37:13 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -7,13 +7,13 @@ end
 
 def second_challenge
   epic_tragedy = {
-     :montague = {
+     :montague => {
         :patriarch => {},
         :matriarch => {},
         :hero => {},
         :hero_friends => []
      },
-     :capulet = {
+     :capulet => {
         :patriarch => {},
         :matriarch => {},
         :heroine => {},
